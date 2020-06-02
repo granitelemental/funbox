c763136262d8e88d59bc38146a5f2a88e49e3b39
Alena Zhukova <a.zhukova@fzlabs.ru>
2020-06-02 12:14:38 +0300

    pep8 corrected

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timezone
-import tldextract
-import validators
 
 from flask import Flask, jsonify, request, Response
 import redis
+import tldextract
+import validators
+
 
 app = Flask('API')
 redis = redis.Redis()
@@ -20,6 +21,11 @@ def check_valid_domain(link):
     return domain
 
 
+def list_isinstance(lst, tp):
+    res = list(map(lambda x: isinstance(x, tp), lst))
+    return all(res)
+
+
 @app.route("/visited_links", methods=["POST"])
 def add_domains():
     try:
@@ -29,7 +35,8 @@ def add_domains():
         assert "links" in json, "Trere is no field 'links' in data"
         assert isinstance(json["links"], list), "'links' is not a list"
         assert json["links"] != [], "'links' list is empty"
-        assert all(list(map(lambda x: isinstance(x, str), json["links"]))), "'links' contain non-string values"
+        assert list_isinstance(json["links"], str), (
+            "'links' contain non-string values")
         domains = [check_valid_domain(link) for link in json["links"]]
     except Exception as e:
         return {"status": str(e)}, 400
@@ -42,22 +49,27 @@ def add_domains():
     except Exception as e:
         return {"status": str(e)}, 500
     return {"status": "ok"}
-    
+
 
 @app.route("/visited_domains", methods=["GET"])
 def get_domains():
     try:
         start = request.args.get("from", "-inf")
         end = request.args.get("to", "+inf")
-        assert not (start != "-inf" and end != "+inf") ^ (start.isdigit() and end.isdigit()), "start/end is not a digit"
-        assert not (start != "-inf" and end != "+inf" and int(start) > int(end)), "'from' is greater than 'to'"
+        staer_end_passed = start != "-inf" and end != "+inf"
+        assert not staer_end_passed ^ (start.isdigit() and end.isdigit()), (
+            "start/end is not a digit")
+        assert not staer_end_passed and int(start) > int(end), (
+            "'from' is greater than 'to'")
     except Exception as e:
         return {"status": str(e)}, 400
 
     try:
         assert is_db_connected, "no connection to redis db"
-        assert redis.exists("visited_links"), "there is no such key in redis db"
-        assert redis.zrangebyscore("visited_links", '-inf', '+inf'), "sorted set is empty"
+        assert redis.exists("visited_links"), (
+            "there is no such key in redis db")
+        assert redis.zrangebyscore("visited_links", '-inf', '+inf'), (
+            "sorted set is empty")
         res = redis.zrangebyscore("visited_links", start, end)
         res = [str(x, "utf-8").split(":")[-1] for x in res]
         res = list(set(res))
--- flask_test.py
+++ flask_test.py
@@ -1,49 +1,58 @@
 import requests
 
+
 class TestPost:
-    post_url = "http://localhost:8080/visited_links"
+    url = "http://localhost:8080/visited_links"
 
     def test_post_ok(self):
-        json = {"links":[
-                "ya.ru", 
-                "https://ya.ru", 
-                "https://ya.ru?q=123", 
-                "https://stackoverflow.com/questions/11828270/how-to-exit-the-vim-editor"
+        json = {"links": [
+                "ya.ru",
+                "https://ya.ru",
+                "https://ya.ru?q=123",
+                "https://stackoverflow.com/questions/10827160/undo-a-git-stash"
                 ]}
-        response = requests.post(url=self.post_url, json=json)
-        assert (response.json(), response.status_code) == ({"status": "ok"}, 200)
-        
+        response = requests.post(url=self.url, json=json)
+        result = response.json(), response.status_code
+        assert result == ({"status": "ok"}, 200)
+
     def test_post_no_data(self):
-        response = requests.post(url=self.post_url, json=None)
-        assert (response.json(), response.status_code) == ({"status": "there is no data"}, 400)
+        response = requests.post(url=self.url, json=None)
+        result = response.json(), response.status_code
+        assert result == ({"status": "there is no data"}, 400)
 
     def test_post_not_json_format(self):
         data = [1, [], "", (1,)]
         for d in data:
-            response = requests.post(url=self.post_url, json=d)
-            assert (response.json(), response.status_code) == ({"status": "Data is not a dict"}, 400)
-            
+            response = requests.post(url=self.url, json=d)
+            result = response.json(), response.status_code
+            assert result == ({"status": "Data is not a dict"}, 400)
+
     def test_post_not_no_links(self):
-        response = requests.post(url=self.post_url, json={})
-        assert (response.json(), response.status_code) == ({"status": "Trere is no field 'links' in data"}, 400)
+        response = requests.post(url=self.url, json={})
+        result = response.json(), response.status_code
+        assert result == ({"status": "Trere is no field 'links' in data"}, 400)
 
     def test_post_links_not_list(self):
         data = [{"links": 1}, {"links": ""}, {"links": {}}, {"links": None}]
         for d in data:
-            response = requests.post(url=self.post_url, json=d)
-            assert (response.json(), response.status_code) == ({"status": "'links' is not a list"}, 400)
-        
+            response = requests.post(url=self.url, json=d)
+            result = response.json(), response.status_code
+            assert result == ({"status": "'links' is not a list"}, 400)
+
     def test_post_links_empty(self):
         json = {"links": []}
-        response = requests.post(url=self.post_url, json=json)
-        assert (response.json(), response.status_code) == ({"status": "'links' list is empty"}, 400)
+        response = requests.post(url=self.url, json=json)
+        result = response.json(), response.status_code
+        assert result == ({"status": "'links' list is empty"}, 400)
 
     def test_post_links_non_string(self):
         data = [{"links": [1]}, {"links": [[]]}, {"links": [(1,)]}]
         for d in data:
-            response = requests.post(url=self.post_url, json=d)
-            assert (response.json(), response.status_code) == ({"status": "'links' contain non-string values"}, 400)
-        
+            response = requests.post(url=self.url, json=d)
+            result = response.json(), response.status_code
+            assert result == (
+                {"status": "'links' contain non-string values"}, 400)
+
     def test_post_invalid_domain(self):
         data = [
             {"links": ["..."]},
@@ -52,29 +61,26 @@ class TestPost:
             {"links": ["abc://ya.ru"]},
             {"links": ["https://yaru"]}]
         for d in data:
-            response = requests.post(url=self.post_url, json=d)
-            assert (response.json(), response.status_code) == ({"status": "'links' contain an invalid link"}, 400)
+            response = requests.post(url=self.url, json=d)
+            result = response.json(), response.status_code
+            assert result == (
+                {"status": "'links' contain an invalid link"}, 400)
+
 
 class TestGet:
-    get_url = "http://localhost:8080/visited_domains"
+    url = "http://localhost:8080/visited_domains"
 
     def test_valid_from_to(self):
         data = [{"start": "a", "end": 1}, {"start": 1, "end": "a"}]
         for d in data:
-            response = requests.get(url=self.get_url + f"?from={d['start']}&to={d['end']}")
-            assert (response.json(), response.status_code) == ({"status": "start/end is not a digit"}, 400)
+            url = self.url + f"?from={d['start']}&to={d['end']}"
+            response = requests.get(url=url)
+            result = (response.json(), response.status_code)
+            assert result == ({"status": "start/end is not a digit"}, 400)
 
     def test_from_greater_than_to(self):
-        start = 2
-        end = 1
-        response = requests.get(url=self.get_url + f"?from={start}&to={end}")
-        assert (response.json(), response.status_code) == ({"status": "'from' is greater than 'to'"}, 400)
-
-    
-
-
-
-
-
-
-  
+        start, end = 2, 1
+        url = self.url + f"?from={start}&to={end}"
+        response = requests.get(url=url)
+        result = (response.json(), response.status_code)
+        assert result == ({"status": "'from' is greater than 'to'"}, 400)
