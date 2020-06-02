50be4ddbeef1d7e70348c96e4fbb06fa0e42ff5e
Alena Zhukova <a.zhukova@fzlabs.ru>
2020-06-02 19:38:12 +0300

    pep8 added

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 ### Usage
 
-1) DB: docker-compose up db
-2) Requirements: pip install -r requirements.txt
-3) API: python api.py
-4) Requests:
+1) Build a docker-image for api: docker-compose build
+2) Run Redis db: docker-compose up db
+3) Run api: docker-compose up api
+4) Requests to api:
 
         a) /visited_links - posts domains into database. 
         Request body format: 
@@ -18,12 +18,13 @@
         b) /visited_domains - gets domain names wisited in certain period.
         Agruments: "from", "to" - utc timestamps. Default: "-inf", "+inf".
 
-4) Tests: pytest
+5) Tests: python flask_test.py
 
         TestPost class- tests for saving visited domains,
         TestsGet class- tests for getting visited domains.
 
 
 ### Comments
-До этого задания я не имела дела с юнит-тестами, так что код может выглядеть странно.
+У меня есть начальные навыки работы с docker-compose, поэтому я подумала, что было бы удобно иметь возможность запускать api на сервере без Python. 
+Но до этого задания я не имела дела с юнит-тестами и не знаю, как их использовать с docker-compose, поэтому запуск тестов осуществляется просто строкой "python flask_test.py".
     
--- flask_test.py
+++ flask_test.py
@@ -13,39 +13,46 @@ class TestPost(unittest.TestCase):
                 "https://stackoverflow.com/questions/10827160/undo-a-git-stash"
                 ]}
         response = requests.post(url=self.url, json=json)
-        self.assertTrue(response.json(), response.status_code) == ({"status": "ok"}, 200)
-        
+        self.assertTrue(response.json(), response.status_code) == (
+            {"status": "ok"}, 200)
+
     def test_post_no_data(self):
         response = requests.post(url=self.url, json=None)
-        self.assertTrue(response.json(), response.status_code) == ({"status": "there is no data"}, 400)
+        self.assertTrue(response.json(), response.status_code) == (
+            {"status": "there is no data"}, 400)
 
     def test_post_not_json_format(self):
         data = [1, [], "", (1,)]
         for d in data:
             response = requests.post(url=self.url, json=d)
-            self.assertTrue(response.json(), response.status_code) == ({"status": "Data is not a dict"}, 400)
-            
+            self.assertTrue(response.json(), response.status_code) == (
+                {"status": "Data is not a dict"}, 400)
+
     def test_post_not_no_links(self):
         response = requests.post(url=self.url, json={})
-        self.assertTrue(response.json(), response.status_code) == ({"status": "Trere is no field 'links' in data"}, 400)
+        self.assertTrue(response.json(), response.status_code) == (
+            {"status": "Trere is no field 'links' in data"}, 400)
 
     def test_post_links_not_list(self):
         data = [{"links": 1}, {"links": ""}, {"links": {}}, {"links": None}]
         for d in data:
             response = requests.post(url=self.url, json=d)
-            self.assertTrue(response.json(), response.status_code) == ({"status": "'links' is not a list"}, 400)
-        
+            self.assertTrue(response.json(), response.status_code) == (
+                {"status": "'links' is not a list"}, 400)
+
     def test_post_links_empty(self):
         json = {"links": []}
         response = requests.post(url=self.url, json=json)
-        self.assertTrue(response.json(), response.status_code) == ({"status": "'links' list is empty"}, 400)
+        self.assertTrue(response.json(), response.status_code) == (
+            {"status": "'links' list is empty"}, 400)
 
     def test_post_links_non_string(self):
         data = [{"links": [1]}, {"links": [[]]}, {"links": [(1,)]}]
         for d in data:
             response = requests.post(url=self.url, json=d)
-            self.assertTrue(response.json(), response.status_code) == ({"status": "'links' contain non-string values"}, 400)
-        
+            self.assertTrue(response.json(), response.status_code) == (
+                {"status": "'links' contain non-string values"}, 400)
+
     def test_post_invalid_domain(self):
         data = [
             {"links": ["..."]},
@@ -55,7 +62,8 @@ class TestPost(unittest.TestCase):
             {"links": ["https://yaru"]}]
         for d in data:
             response = requests.post(url=self.url, json=d)
-            self.assertTrue(response.json(), response.status_code) == ({"status": "'links' contain an invalid link"}, 400)
+            self.assertTrue(response.json(), response.status_code) == (
+                {"status": "'links' contain an invalid link"}, 400)
 
 
 class TestGet(unittest.TestCase):
@@ -64,22 +72,18 @@ class TestGet(unittest.TestCase):
     def test_valid_from_to(self):
         data = [{"start": "a", "end": 1}, {"start": 1, "end": "a"}]
         for d in data:
-            response = requests.get(url=self.url + f"?from={d['start']}&to={d['end']}")
-            self.assertTrue(response.json(), response.status_code) == ({"status": "start/end is not a digit"}, 400)
+            response = requests.get(
+                url=self.url + f"?from={d['start']}&to={d['end']}")
+            self.assertTrue(response.json(), response.status_code) == (
+                {"status": "start/end is not a digit"}, 400)
 
     def test_from_greater_than_to(self):
         start = 2
         end = 1
         response = requests.get(url=self.url + f"?from={start}&to={end}")
-        self.assertTrue(response.json(), response.status_code) == ({"status": "'from' is greater than 'to'"}, 400)
+        self.assertTrue(response.json(), response.status_code) == (
+            {"status": "'from' is greater than 'to'"}, 400)
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
-
-
-  
